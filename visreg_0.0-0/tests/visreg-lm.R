0023a234c46f9f986387b869e97736728de5477c
Patrick Breheny <patrick.breheny@uky.edu>
2012-06-20 12:03:01 -0400

    Removed obsolete files

--- visreg_0.0-0/tests/visreg-lm.R
+++
@@ -1,8 +0,0 @@
-ozone <- airquality[complete.cases(airquality),]
-
-fit <- lm(Ozone ~ Solar.R + Wind + Temp,data=ozone)
-visreg(fit,"Wind")
-visreg(fit,c("Solar.R","Wind","Temp"))
-
-fit <- lm(Ozone ~ Solar.R + Wind + Temp + I(Wind^2),data=ozone)
-visreg(fit,"Wind")
--- visreg_0.3-0/R/centerFrame.R
+++
@@ -1,8 +0,0 @@
-centerFrame <- function(fit,f,name)
-  {
-    f[,name] <- f[,name] - mean(f[,name])
-    x <- as.formula(paste(formula(fit)[2],"~",formula(fit)[3]))
-    df <- model.frame(as.formula(paste(formula(fit)[2],"~",formula(fit)[3])),f)
-    df <- cbind(df,f[,setdiff(names(f),names(df))])
-    return(df)
-  }
