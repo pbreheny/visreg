90240b1e888fb9c38b1d8b2d3e5f37f81b11d889
Patrick Breheny <patrick.breheny@uky.edu>
2013-01-20 10:19:32 -0500

    Uploaded version 1.2-0 to CRAN

--- R/getXY.R
+++ R/getXY.R
@@ -2,8 +2,7 @@ getXY <- function(fit, f, name, nn, cond, type, trans, xtrans, alpha, jitter)
 {
   if (type=="conditional") {
     x <- setupD(fit,f,name,nn,cond)
-    y <- if ("coxph" %in% class(fit)) Response.coxph(fit, x, trans, alpha) else
-      Response(fit,x,trans,alpha)
+    y <- Response(fit,x,trans,alpha)
   } else if (type=="effect") {
     x <- setupX(fit, f, name, nn, cond)
     y <- Terms(fit,f,x,trans,alpha)
