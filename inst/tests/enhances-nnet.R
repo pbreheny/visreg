0a2dea7a4b0ae3b36f50d7b815df8724a2f85703
Patrick Breheny <patrick-breheny@uiowa.edu>
2016-08-05 09:44:15 -0500

    Minor extension to multinom testing

--- inst/tests/enhances-nnet.R
+++ inst/tests/enhances-nnet.R
@@ -2,4 +2,6 @@ if(require(nnet)) {
   airquality$Heat <- cut(airquality$Temp,3,labels=c("Cool","Mild","Hot"))
   fit <- multinom(Heat ~ Wind + Ozone, airquality)
   visreg(fit, "Ozone", collapse=TRUE, overlay=TRUE, ylab="Probability", ylim=c(0,1), partial=FALSE, rug=2)
+  v <- visreg(fit, "Ozone", plot=FALSE)
+  plot(v[[2]], ylab="Probability", ylim=c(0,1), partial=FALSE, rug=2)
 }
