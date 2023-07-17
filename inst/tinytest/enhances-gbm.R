11f3e531c35885f5d07fd90a06b48f9834b26131
Patrick Breheny <patrick-breheny@uiowa.edu>
2023-07-17 16:33:59 -0500

    Using registerS3Method instead of automatic dispatch

--- inst/tinytest/enhances-gbm.R
+++ inst/tinytest/enhances-gbm.R
@@ -6,7 +6,9 @@ Data <- airquality[complete.cases(airquality),]
 fit <- gbm(Ozone ~ ., data=Data, distribution='gaussian')
 expect_warning(visreg(fit, 'Wind'))
 visreg(fit, 'Wind', partial=FALSE, rug=FALSE)
-residuals.gbm <- function(fit) Data$Ozone - fit$fit  # supplying a resid() function
+
+# Supply our own residuals() function
+registerS3method('residuals', 'gbm', function(fit) Data$Ozone - fit$fit)
 visreg(fit, 'Wind')
 fit <- gbm(Ozone ~ ., data=Data, n.trees=10000, distribution='gaussian')
 visreg(fit, 'Wind')
@@ -16,6 +18,6 @@ data("birthwt", package="MASS")
 Data <- birthwt[,1:9]
 Data$race <- factor(Data$race,labels=c("White","Black","Other"))
 Data$smoke <- factor(Data$smoke,labels=c("Nonsmoker","Smoker"))
-fit <- gbm(low ~ age + lwt, data=Data, n.trees = 10000, , distribution='bernoulli')
-residuals.gbm <- function(fit) Data$low
+fit <- gbm(low ~ age + lwt, data=Data, n.trees = 10000, distribution='bernoulli')
+registerS3method('residuals', 'gbm', function(fit) Data$low)
 visreg(fit, 'lwt', trans=binomial()$linkinv, ylim=0:1, rug=2)
