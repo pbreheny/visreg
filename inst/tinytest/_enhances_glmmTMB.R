ef81ce34f6745457ba40160291695889d4e1641e
Patrick Breheny <patrick-breheny@uiowa.edu>
2024-05-21 08:21:59 -0500

    enhances_glmmTMB: adding autocorrelation check

--- inst/tinytest/_enhances_glmmTMB.R
+++ inst/tinytest/_enhances_glmmTMB.R
@@ -4,6 +4,17 @@ Owls <- transform(
   Nest=reorder(Nest,NegPerChick),
   NCalls=SiblingNegotiation,
   FT=FoodTreatment)
+
+# A regular model
+fit <- glmmTMB(
+  NCalls ~ FT + ArrivalTime + offset(log(BroodSize)) + (1|Nest),
+  data=Owls, family=poisson)
+visreg(fit, 'FT')
+visreg(fit, 'FT', type='contrast')
+visreg(fit, 'ArrivalTime')
+visreg(fit, 'ArrivalTime', type='contrast')
+
+# Now with zero inflation
 fit <- glmmTMB(
   NCalls ~ FT + ArrivalTime + offset(log(BroodSize)) + (1|Nest),
   data=Owls, ziformula=~SexParent, family=poisson)
@@ -12,10 +23,12 @@ visreg(fit, 'FT', type='contrast')
 visreg(fit, 'ArrivalTime')
 visreg(fit, 'ArrivalTime', type='contrast')
 
-fit <- glmmTMB(
-  NCalls ~ FT + ArrivalTime + offset(log(BroodSize)) + (1|Nest),
-  data=Owls, family=poisson)
-visreg(fit, 'FT')
-visreg(fit, 'FT', type='contrast')
-visreg(fit, 'ArrivalTime')
-visreg(fit, 'ArrivalTime', type='contrast')
+# Autocorrelation
+n <- 25
+x <- MASS::mvrnorm(mu = rep(0,n), Sigma = .7 ^ as.matrix(dist(1:n)) )
+y <- x + rnorm(n)
+times <- factor(1:n, levels=1:n)
+group <- factor(rep(1,n))
+dat <- data.frame(y, times, group)
+fit <- glmmTMB(y ~ ar1(times + 0 | group), data=dat)
+visreg(fit, 'times')
