ae65635ae0bf052f04e50ee999b42b8da224bb0d
Patrick Breheny <patrick-breheny@uiowa.edu>
2026-07-14 09:54:17 -0500

    Lazy-register S3 generics
    Previous setup led to problems when running automated checks (GitHub
    actions).

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,20 @@
 .onLoad <- function(libname, pkgname) {
-  if (requireNamespace("rgl", quietly = TRUE)) {
-    registerS3method("persp3d", "visreg2d", persp3d.visreg2d, envir = asNamespace("rgl"))
-    registerS3method("persp3d", "visreg_list", persp3d.visreg_list, envir = asNamespace("rgl"))
+  register_s3_method("rgl", "persp3d", "visreg2d", persp3d.visreg2d)
+  register_s3_method("rgl", "persp3d", "visreg_list", persp3d.visreg_list)
+}
+
+# Register an S3 method for suggests-only package without forcing it to load. In
+# particular, this is an issue with rgl. If rgl is already loaded, register now;
+# if not, arrange for registration to happen if/when the user loads rgl
+# themselves.
+register_s3_method <- function(pkg, generic, class, fun) {
+  if (pkg %in% loadedNamespaces()) {
+    registerS3method(generic, class, fun, envir = asNamespace(pkg))
   }
+  setHook(
+    packageEvent(pkg, "onLoad"),
+    function(...) registerS3method(generic, class, fun, envir = asNamespace(pkg))
+  )
 }
 
 .onAttach <- function(libname, pkgname) {
